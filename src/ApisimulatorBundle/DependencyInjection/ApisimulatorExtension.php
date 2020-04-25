e61c3f65e7538cdf73ba324a17cc612dc2d311e8
alhuo-smile <alexandre.huon@smile.fr>
2020-04-25 21:03:18 +0200

    Custopm warnings

--- src/ApisimulatorBundle/DependencyInjection/ApisimulatorExtension.php
+++ src/ApisimulatorBundle/DependencyInjection/ApisimulatorExtension.php
@@ -19,11 +19,29 @@ class ApisimulatorExtension extends Extension
             $container->setParameter('apisimulator.guards.methods', $config['guards']['methods']);
             $container->setParameter('apisimulator.guards.status_codes', $config['guards']['status_codes']);
             $container->setParameter('apisimulator.guards.paths', $config['guards']['paths']);
+        } else {
+            // Ugly, refine later
+            $container->setParameter('apisimulator.guards.methods', []);
+            $container->setParameter('apisimulator.guards.status_codes', []);
+            $container->setParameter('apisimulator.guards.paths', []);
         }
 
         if (!empty($config['headers_removal'])) {
             $container->setParameter('apisimulator.headers_removal.request', $config['headers_removal']['request']);
             $container->setParameter('apisimulator.headers_removal.response', $config['headers_removal']['response']);
+        } else {
+            // Ugly, refine later
+            $container->setParameter('apisimulator.headers_removal.request', []);
+            $container->setParameter('apisimulator.headers_removal.response', []);
+        }
+
+        if (!empty($config['warnings'])) {
+            $container->setParameter('apisimulator.warnings.request_headers', $config['warnings']['request_headers']);
+            $container->setParameter('apisimulator.warnings.request_body', $config['warnings']['request_body']);
+        } else {
+            // Ugly, refine later
+            $container->setParameter('apisimulator.warnings.request_headers', []);
+            $container->setParameter('apisimulator.warnings.request_body', []);
         }
 
         $loader = new YamlFileLoader($container, new FileLocator(dirname(__DIR__).'/Resources/config'));
